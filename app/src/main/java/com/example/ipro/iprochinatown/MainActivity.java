6f4ac41e7f6c4d115d72fc3d78c6ed998a06cd41
jingwenyu <jyu57@hawk.iit.edu>
2018-09-26 11:30:08 -0500

    Started basic layout

---
+++ app/src/main/java/com/example/ipro/iprochinatown/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ipro.iprochinatown;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+}
